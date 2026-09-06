0a258cb54144e0808d717412d465ad9d6f3e832e
tamaslin-creator <tamas.lin@gmail.com>
2026-09-06 21:54:51 +0800

    Fix API endpoint path for queryWord function

--- app/core/apis/words.ts
+++ app/core/apis/words.ts
@@ -2,7 +2,9 @@ import http from '../utils/http.ts'
 import type { Word } from '../types'
 
 export function queryWord(params?: { word: string }) {
-  return http<Word>('public.word/query', null, params, 'get')
+  return http<Word>('/public.word/query', null, params, 'get')
 }
+
 export function getWordList(params?, data?) {
   return http('public.word/getWordList', data, params, 'post')
+}
