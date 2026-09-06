3e8a9b89ca4e2d716969e40fe09635fb8de448b2
tamaslin-creator <tamas.lin@gmail.com>
2026-09-06 21:49:32 +0800

    Add getWordList function to query word list

--- app/core/apis/words.ts
+++ app/core/apis/words.ts
@@ -4,3 +4,5 @@ import type { Word } from '../types'
 export function queryWord(params?: { word: string }) {
   return http<Word>('public.word/query', null, params, 'get')
 }
+export function getWordList(params?, data?) {
+  return http('public.word/getWordList', data, params, 'post')
